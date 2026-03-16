cec2a942220a8808b0c7948ff322298e383acf24
mariemboussaada <mariem.bousaada@fsb.ucar.tn>
2026-03-16 13:37:18 +0100

    amelioration3

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -11,12 +11,31 @@ class CheckoutPage:
     continue_btn = (By.ID, "continue")
     finish_btn   = (By.ID, "finish")
     confirmation = (By.CSS_SELECTOR, ".complete-header")
-    summary      = (By.ID, "checkout_summary_container")   # ← page step-two
-    complete     = (By.CSS_SELECTOR, ".checkout_complete_container")  # ← page complete
+    summary      = (By.ID, "checkout_summary_container")
+    complete     = (By.CSS_SELECTOR, ".checkout_complete_container")
 
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 30)
+
+    def _fill_field(self, locator, value):
+        """Remplit un champ React de façon fiable en headless."""
+        field = self.wait.until(EC.presence_of_element_located(locator))
+        self.driver.execute_script("arguments[0].click();", field)
+        self.driver.execute_script("arguments[0].value = '';", field)
+        field.send_keys(value)
+        actual = self.driver.execute_script("return arguments[0].value;", field)
+        if actual != value:
+            self.driver.execute_script(
+                """
+                var setter = Object.getOwnPropertyDescriptor(
+                    window.HTMLInputElement.prototype, 'value').set;
+                setter.call(arguments[0], arguments[1]);
+                arguments[0].dispatchEvent(new Event('input', { bubbles: true }));
+                arguments[0].dispatchEvent(new Event('change', { bubbles: true }));
+                """,
+                field, value
+            )
 
     def checkout(self):
         btn = self.wait.until(EC.presence_of_element_located(self.checkout_btn))
@@ -24,32 +43,19 @@ class CheckoutPage:
         self.wait.until(EC.presence_of_element_located(self.first_name))
 
     def fill_information(self, fname, lname, zip_code):
-        fn = self.wait.until(EC.presence_of_element_located(self.first_name))
-        fn.click()
-        fn.clear()
-        fn.send_keys(fname)
-
-        ln = self.driver.find_element(*self.last_name)
-        ln.click()
-        ln.clear()
-        ln.send_keys(lname)
-
-        pc = self.driver.find_element(*self.postal_code)
-        pc.click()
-        pc.clear()
-        pc.send_keys(zip_code)
+        self._fill_field(self.first_name, fname)
+        self._fill_field(self.last_name, lname)
+        self._fill_field(self.postal_code, zip_code)
 
     def continue_checkout(self):
         btn = self.wait.until(EC.presence_of_element_located(self.continue_btn))
         self.driver.execute_script("arguments[0].click();", btn)
-
         self.wait.until(EC.presence_of_element_located(self.summary))
 
     def finish_checkout(self):
         finish = self.wait.until(EC.presence_of_element_located(self.finish_btn))
         self.driver.execute_script("arguments[0].scrollIntoView(true);", finish)
         self.driver.execute_script("arguments[0].click();", finish)
-
         self.wait.until(EC.presence_of_element_located(self.complete))
 
     def get_confirmation_text(self):
